6d1528f2938a2436bbf12be75a959f6c6fc1f2cc
Nyukers Ltd. <nyukers@gmail.com>
2024-09-29 15:23:45 +0300

    Add files via upload

---
+++ Fooocus/run.bat
@@ -0,0 +1,8 @@
+nvidia-smi
+:: Update
+::.\python_embeded\python.exe -s Fooocus\entry_with_update.py --theme=dark --preset default %*
+::
+:: Skip update
+.\python_embeded\python.exe -s Fooocus\launch.py --theme=dark --preset default %*
+
+pause
