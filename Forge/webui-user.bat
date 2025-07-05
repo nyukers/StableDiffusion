9ec5720e262bc15d05f4ab611a73a7b7846d1f9e
Nyukers Pro <nyukers@gmail.com>
2025-07-05 12:32:46 +0300

    Add files via upload

--- Forge/webui-user.bat
+++ Forge/webui-user.bat
@@ -1,18 +1,26 @@
 @echo off
+nvidia-smi
 
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--theme=dark
+set MODEL_HOME=D:/models
+set A1111_HOME=D:/SDPortable
+set VENV_DIR=%A1111_HOME%/venv
+:: set HF_HUB_OFFLINE=True
+:: set XFORMERS_MORE_DETAILS=1
 
-@REM Uncomment following code to reference an existing A1111 checkout.
-@REM set A1111_HOME=Your A1111 checkout dir
-@REM
-@REM set VENV_DIR=%A1111_HOME%/venv
-@REM set COMMANDLINE_ARGS=%COMMANDLINE_ARGS%^
-@REM  --ckpt-dir %A1111_HOME%/models/Stable-diffusion^
-@REM  --hypernetwork-dir %A1111_HOME%/models/hypernetworks^
-@REM  --embeddings-dir %A1111_HOME%/models/embeddings^
-@REM  --lora-dir %A1111_HOME%/models/Lora
+set COMMANDLINE_ARGS=--xformers --theme=dark --styles-file="D:\Addon\Styles\*.csv" --cuda-malloc
+::--forge-ref-a1111-home %A1111_HOME% 
+::--cuda-malloc --cuda-stream 
+
+set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% ^
+--ckpt-dir %MODEL_HOME%/Stable-diffusion ^
+--hypernetwork-dir %MODEL_HOME%/hypernetworks ^
+--embeddings-dir %MODEL_HOME%/embeddings ^
+--lora-dir %MODEL_HOME%/Lora ^
+--vae-dir %MODEL_HOME%/vae
+
+echo # %COMMANDLINE_ARGS%
 
 call webui.bat
